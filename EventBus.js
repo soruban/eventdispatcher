e1894715b46b87d654c358b11887786f9b63631b
soruban <soruban@gmail.com>
2013-05-01 22:47:54 +0100

    EventBus (wip). Added the notion of evenChannel on the bus.

--- EventBus.js
+++ EventBus.js
@@ -1,13 +1,17 @@
 (function(ns){
     var UID = 0;
     var UID_FIELD = "_uid";
+    var DEFAULT_CHANNEL = "default_channel";
 
     /**
-     * Provides a centralized Bus for listening and dispatching events.
-     * The bus allows any object to dispatch/publish event and any other object to listen for those.
+     * An event channel allows is a 'dedicated bus' with an id that allows obejct to dispatch and received events.
+     * It should not be created explicitly, the event bus will create and dispose of event channels as needed.
      * @constructor
      */
-    var EventBus = function() {
+    var EventChannel = function(channelId) {
+        if(!channelId) {
+            channelId = new Date().getMilliseconds() + ""  + Math.round(Math.random() * 1000);
+        }
         this._currTargets = {};
     };
 
@@ -18,7 +22,7 @@
      * @param {Function} callback , the callback to trigger when the event is triggered on the target.
      * @param {Object} context , the context to use in the callback.
      */
-    EventBus.prototype.listen = function(target, eventName, callback, context) {
+    EventChannel.prototype.listen = function(target, eventName, callback, context) {
         var uid, listenersForTarget, listenersForName;
 
         // Give a UID for the target if it does not have one.
@@ -43,7 +47,7 @@
         });
     };
 
-    EventBus.prototype.unlisten = function(target, eventName, callback, context) {
+    EventChannel.prototype.unlisten = function(target, eventName, callback, context) {
         if(target[UID_FIELD] && this._currTargets[UID_FIELD] && this._currTargets[UID_FIELD][eventName]) {
             var listeners = this._currTargets[UID_FIELD][eventName];
             var count = listeners.count;
@@ -58,8 +62,8 @@
         }
     };
 
-    EventBus.prototype.dispatch = function(target, event) {
-        if(target[UID_FIELD] && this._currTargets[UID_FIELD] && this._currTargets[UID_FIELD][eventName]) {
+    EventChannel.prototype.dispatch = function(target, event) {
+        if(!this._muted && target[UID_FIELD] && this._currTargets[UID_FIELD] && this._currTargets[UID_FIELD][eventName]) {
             var listeners = this._currTargets[UID_FIELD][eventName];
             var count = listeners.count;
             for(var i = 0; i < count; i++) {
@@ -68,7 +72,117 @@
         }
     };
 
-    EventBus.prototype._getUID = function() {
+    EventChannel.prototype.mute = function() {
+        this._muted = true;
+    };
+
+    EventChannel.prototype.unmute = function() {
+        this._muted = false;
+    };
+
+    EventChannel.prototype._getUID = function() {
         return UID++;
     };
+
+
+    /**
+     * Provides a centralized Bus for listening and dispatching events.
+     * The bus allows any object to dispatch/publish event and any other object to listen for those.
+     * This implementation makes uses of "channels" to allow users to create dedicated channels for grouping various events.
+     * Objects may create event channels and dispatch to object on the specific channel only.
+     * By default all event are dispatched on the default channel.
+     * @constructor
+     */
+    var EventBus = function() {
+        this._channels = {};
+        this._channels[DEFAULT_CHANNEL] = new EventChannel(DEFAULT_CHANNEL);
+    };
+
+    /**
+     * Sets up a listener for the eventName on target, the given callback will be called.
+     * @param {Object} target
+     * @param {String} eventName
+     * @param {Function} callback
+     * @param {Object} context
+     * @param {String=} channel , optional, the name of the channel to set the listen on.
+     * @throws {Error} if the channel does not exist.
+     */
+    EventBus.prototype.listen = function(target, eventName, callback, context, channel) {
+        if(channel && !this._channels[id]) {
+            throw new Error("Channel does not exists");
+        }
+        else {
+            channel = channel || DEFAULT_CHANNEL;
+            this._channels[channel].listen(target, eventName, callback);
+        }
+    };
+
+    /**
+     * @param {Object} target
+     * @param {String} eventName
+     * @param {Function} callback
+     * @param {Object} context
+     * @param {String=} channel , optional, the name of the channel to set the listen on.
+     * @returns {Boolean} true if we managed to successfully remoe the event, false otherwise.
+     */
+    EventBus.prototype.unlisten = function(target, eventName, callback, context, channel) {
+        if(channel && !this._channels[id]) {
+            return false;
+        }
+        else {
+            channel = channel || DEFAULT_CHANNEL;
+            return this._channels[channel].unlisten(target, eventName, callback, context);
+        }
+    };
+
+    /**
+     * @param {Object} target
+     * @param {String} eventName
+     * @param {String=} channel , optional, the name of the channel to set the listen on.
+     * @throws {Error} if the channel does not exist.
+     */
+    EventBus.prototype.dispatch = function(target, eventName, channel) {
+        if(channel && !this._channels[id]) {
+            throw new Error("Channel does not exists");
+        }
+        else {
+            channel = channel || DEFAULT_CHANNEL;
+            return this._channels[channel].dispatch(target, eventName, callback);
+        }
+    };
+
+    /**
+     * Creates a dedicated channel with the given id. This id can then be used in the listen, unlisten and dispatch
+     * functions.
+     * @param id
+     * @throws {Error} if the channel already exists.
+     */
+    EventBus.prototype.createChannel = function(id) {
+        if(this._channels[id]) {
+            throw new Error("Channel " + id + " already exists.");
+        }
+        else {
+            this._channels[id] = new EventChannel(id);
+        }
+    };
+
+    /**
+     * Returns the channel for the given id if it exists or null.
+     * @param {String} id
+     * @returns {EventChannel}
+     */
+    EventBus.prototype.getChannel = function(id) {
+        return this._channels[id] || null;
+    };
+
+    /**
+     * @param {String} id
+     * @returns {boolean}
+     */
+    EventBus.prototype.hasChannel = function(id) {
+        return !!this._channels[id];
+    };
+
 })(window);
+
+//TODO: Wrap target in Event object? e.g. var ev = new Event("click", myObject)?
